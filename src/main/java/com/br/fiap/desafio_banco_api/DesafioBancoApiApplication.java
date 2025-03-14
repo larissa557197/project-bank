bea093a3c1869b01e35557b6c238fe8c73a64d43
Larissa Muniz <larissampmuniz@gmail.com>
2025-03-14 11:24:49 -0300

    init: configurar estrutura inicial do projeto
    - Criar estrutura base do projeto Spring Boot
    - Adicionar dependências iniciais no pom.xml
    - Configurar estrutura de pacotes

---
+++ src/main/java/com/br/fiap/desafio_banco_api/DesafioBancoApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.desafio_banco_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBancoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/desafio_banco_api/DesafioBancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.desafio_banco_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
